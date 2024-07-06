322292ca7bd175fe0a4216b6fa7b766344037fd8
Abdullah Al Noman <imnomanmohammad19@gmail.com>
2024-07-06 21:56:26 +0600

    First Program
    This is my first program where i printed my name
    
    I have also use comments here as documentation

---
+++ FirstProgram.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+int main() { 
+   printf("Hi, I am Mohammad Noman");
+   return 0;
+}
+//* #include <stdio.h> is a header file library that lets us work with input and output functions, such as printf() (used in line 4). Header files add functionality to C programs.
+//* C ignores white space. But we use it to make the code more readable.
+//* Another thing that always appear in a C program is main(). This is called a function. Any code inside its curly brackets {} will be executed. Also, programme execution has started from main functions.
+//* printf() displays the string inside quotation. printf() is a library function used to output/print text to the screen.
+//* return 0 ends the main() function.
+//* Here I can also use getchar() instead of return 0. getchar command will help removing unnecessery line in outputs.
+//! Do not forget to add the closing curly bracket } to actually end the main function.
